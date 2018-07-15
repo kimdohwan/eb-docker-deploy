9a53fbad1a81662864b2674270d84cd1a65d9dc7
dohwan <dosio0102@gmail.com>
2018-07-16 01:25:02 +0900

    deploy.sh 숙제 작성 전 셋팅
    production.py		RDS DB 연결(eb_docker_deploy_production)
    			DEBUG = True 로 변경
    			django error message 만들기 주석 처리
    			static file setting(no S3, local storage)
    url.py 			+static(MEDIA) 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -16,58 +16,55 @@ WSGI_APPLICATION = 'config.wsgi.production.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
+DATABASES = secrets['DATABASES']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
 
-LOG_DIR = '/var/log/django'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'django.server': {
-            'format': '[%(asctime)s] %(message)s',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'file_error': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'level': 'ERROR',
-            'formatter': 'django.server',
-            'backupCount': 10,
-            'filename': os.path.join(LOG_DIR, 'error.log'),
-            'maxBytes': 10485760,
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file_error'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    }
-}
+# LOG_DIR = '/var/log/django'
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'filters': {
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse',
+#         },
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         },
+#     },
+#     'formatters': {
+#         'django.server': {
+#             'format': '[%(asctime)s] %(message)s',
+#         }
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'INFO',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#         },
+#         'file_error': {
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'level': 'ERROR',
+#             'formatter': 'django.server',
+#             'backupCount': 10,
+#             'filename': os.path.join(LOG_DIR, 'error.log'),
+#             'maxBytes': 10485760,
+#         }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console', 'file_error'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         }
+#     }
+# }
